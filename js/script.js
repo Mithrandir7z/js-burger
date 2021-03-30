06c717cc5bfad0730e673e0b72a7dbdd0c241ecb
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-03-30 16:35:43 +0200

    sviluppo algoritmo

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//Al click del bottone parte una funzione che calcola il prezzo ed eventuale sconto coupon
+//Ho un prezzo standard di 50$
+//Se l'utente clicka sui checkbox sommare al prezzo standard
+
+
+//Creare la funzione al click del bottone
+var bottone = document.getElementById("bottone");
+bottone.addEventListener("click", function() {
+
+    var prezzoBase = 50;
+    console.log("PrezzoBase $", prezzoBase);
+});
+
+//Calcolare la somma degli eventuali checkbox
+
+//Calcolare l'eventuale sconto coupon
+
+//Stampare
