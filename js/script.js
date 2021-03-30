f0d566da2daa489bd864e192dfd31c015755986c
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-03-30 17:20:06 +0200

    agg3

--- js/script.js
+++ js/script.js
@@ -7,12 +7,49 @@
 var bottone = document.getElementById("bottone");
 bottone.addEventListener("click", function() {
 
-    var prezzoBase = 50;
-    console.log("PrezzoBase $", prezzoBase);
+    var prezzoTotale = parseInt(50);
+    console.log("PrezzoBase $", prezzoTotale);
+
+    //Calcolare la somma di cheese
+    var cheese = document.getElementById("cheese").value;
+    parseInt(cheese);
+    console.log("Il valore di cheese è: ", cheese);
+
+    //checked cheese
+    var cheeseBoolean = document.getElementById("cheese").checked;
+    console.log("cheeseBool è ", cheeseBoolean);
+
+    if ( cheeseBoolean == true ) {
+
+    }
+
+
+
+
+
+    // var tomato = document.getElementById("tomato").value;
+
+    // var egg = document.getElementById("egg").value;
+
+    // var lettuce = document.getElementById("lettuce").value;
+
+    // var mustard = document.getElementById("mustard").value;
+
+    // var ketchup = document.getElementById("ketchup").value;
+
+    //Verificare se l'utente ha selezionato qualche checkbox, 
+    //se si allora aggiornare il totale. 
+    // if ( cheese.checked == true ) {
+    //     console.log("ho clickato su cheese");
+    // }
+
+    //Stampare
+    
+    
 });
 
-//Calcolare la somma degli eventuali checkbox
+
 
 //Calcolare l'eventuale sconto coupon
 
-//Stampare
+
